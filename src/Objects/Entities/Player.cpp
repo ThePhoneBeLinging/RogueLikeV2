3d937cd70b243404b048b5b6bbc98354245112d9
adel051700 <s235076@dtu.dk>
2024-08-14 23:31:59 +0200

    changed movement speed to 3

--- src/Objects/Entities/Player.cpp
+++ src/Objects/Entities/Player.cpp
@@ -14,16 +14,16 @@ Player::Player() : Entity(50)
     this->mDrawAble.mTextureManager.setTextureIndex(1);
     this->mDrawAble.addToScene(0);
     HotKeyManager::addOnKeyPress(KEY_A, [this](){
-        this->mDrawAble.setX(this->mDrawAble.getX() - 1);
+        this->mDrawAble.setX(this->mDrawAble.getX() - 3);
     }, TriggerContinuously);
     HotKeyManager::addOnKeyPress(KEY_D, [this](){
-        this->mDrawAble.setX(this->mDrawAble.getX() + 1);
+        this->mDrawAble.setX(this->mDrawAble.getX() + 3);
     }, TriggerContinuously);
     HotKeyManager::addOnKeyPress(KEY_W, [this](){
-        this->mDrawAble.setY(this->mDrawAble.getY() - 1);
+        this->mDrawAble.setY(this->mDrawAble.getY() - 3);
     }, TriggerContinuously);
     HotKeyManager::addOnKeyPress(KEY_S, [this](){
-        this->mDrawAble.setY(this->mDrawAble.getY() + 1);
+        this->mDrawAble.setY(this->mDrawAble.getY() + 3);
     }, TriggerContinuously);
 
 }
