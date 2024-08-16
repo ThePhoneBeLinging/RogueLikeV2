f3a28bc10253982eaa7e05c9c8254dc9d7ce24ed
Elias Aggergaard Larsen <s235125@dtu.dk>
2024-08-17 01:14:08 +0200

    Refactored player constructor

--- src/Objects/Entities/Player.cpp
+++ src/Objects/Entities/Player.cpp
@@ -9,25 +9,25 @@ Player::Player() : Entity(50)
 {
     this->mDrawAble.setWidth(50);
     this->mDrawAble.setHeight(50);
-    //
-    // x and y on the player must be set to 0, as the camera is using this value to offset other objects
-    //
     this->mDrawAble.setX(0);
     this->mDrawAble.setY(0);
     this->mDrawAble.mTextureManager.setTextureIndex(1);
     this->mDrawAble.addToScene(0);
-    HotKeyManager::addOnKeyPress(KEY_A, [this](){
+    HotKeyManager::addOnKeyPress(KEY_A, [this]()
+    {
         this->mDrawAble.setX(this->mDrawAble.getX() - 3);
     }, TriggerContinuously);
-    HotKeyManager::addOnKeyPress(KEY_D, [this](){
+    HotKeyManager::addOnKeyPress(KEY_D, [this]()
+    {
         this->mDrawAble.setX(this->mDrawAble.getX() + 3);
     }, TriggerContinuously);
-    HotKeyManager::addOnKeyPress(KEY_W, [this](){
+    HotKeyManager::addOnKeyPress(KEY_W, [this]()
+    {
         this->mDrawAble.setY(this->mDrawAble.getY() - 3);
     }, TriggerContinuously);
-    HotKeyManager::addOnKeyPress(KEY_S, [this](){
+    HotKeyManager::addOnKeyPress(KEY_S, [this]()
+    {
         this->mDrawAble.setY(this->mDrawAble.getY() + 3);
     }, TriggerContinuously);
-
 }
 
